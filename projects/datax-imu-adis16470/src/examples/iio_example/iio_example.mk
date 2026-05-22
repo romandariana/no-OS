92a26bd992b536ba9a8d87cf489f4582c5fc59c8
Alisa-Dariana Roman <alisa.roman@analog.com>
2026-05-22 14:49:35 +0300

    projects: Add datax-imu-adis16470

---
+++ projects/datax-imu-adis16470/src/examples/iio_example/iio_example.mk
@@ -0,0 +1,16 @@
+IIOD=y
+
+CFLAGS += -DNO_OS_NETWORKING
+
+SRC_DIRS += $(NO-OS)/iio/iio_app
+
+INCS += $(DRIVERS)/imu/iio_adis_internals.h \
+	$(DRIVERS)/imu/iio_adis16470.h
+SRCS += $(DRIVERS)/imu/iio_adis.c \
+	$(DRIVERS)/imu/iio_adis16470.c
+
+INCS += $(NO-OS)/network/linux_socket/linux_socket.h \
+	$(NO-OS)/network/tcp_socket.h \
+	$(NO-OS)/network/network_interface.h
+SRCS += $(NO-OS)/network/linux_socket/linux_socket.c \
+	$(NO-OS)/network/tcp_socket.c
